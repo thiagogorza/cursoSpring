b4bcc6d2e76db39a0aeb11e717b609afcbbdb9b5
thiagogorza <thiagomgorza@gmail.com>
2020-08-06 14:04:02 -0300

    ProjetoSpring

---
+++ src/main/java/com/example/demo/resources/ProductResources.java
@@ -0,0 +1,41 @@
+package com.example.demo.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.course.entities.Product;
+import com.example.demo.repositories.ProductRepository;
+import com.example.demo.services.ProductService;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductResources {
+
+	@Autowired
+	private ProductService ProductService;
+
+	@Autowired
+	private ProductRepository ProductRepository;
+
+	@GetMapping
+	public ResponseEntity<List<Product>> findAll() {
+
+		List<Product> Products = ProductRepository.findAll();
+
+		return ResponseEntity.ok().body(Products);
+
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Product> findById(@PathVariable Long id) {
+		Product obj = ProductService.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+}
