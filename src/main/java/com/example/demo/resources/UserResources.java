0206e9049c07b851ffe241a0dec938e7d05fd93e
thiagogorza <thiagomgorza@gmail.com>
2020-08-07 15:56:47 -0300

    ProjetoSpring

--- src/main/java/com/example/demo/resources/UserResources.java
+++ src/main/java/com/example/demo/resources/UserResources.java
@@ -1,13 +1,18 @@
 package com.example.demo.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.demo.course.entities.User;
 import com.example.demo.repositories.UserRepository;
@@ -19,14 +24,14 @@ public class UserResources {
 
 	@Autowired
 	private UserService userService;
-	
+
 	@Autowired
 	private UserRepository userRepository;
 
 	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
-		
-		List<User> users =userRepository.findAll();
+
+		List<User> users = userRepository.findAll();
 
 		return ResponseEntity.ok().body(users);
 
@@ -37,5 +42,19 @@ public class UserResources {
 		User obj = userService.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
+
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = userService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+
+@DeleteMapping(value = "{id}")
+public ResponseEntity<Void> delete(@PathVariable Long id){
+	userService.delete(id);
+	return ResponseEntity.noContent().build();
+}
+
+
 }
