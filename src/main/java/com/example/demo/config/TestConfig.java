d9f092933049d050da83632cd2eebb2ff1c83b0e
thiagogorza <thiagomgorza@gmail.com>
2020-08-07 13:50:35 -0300

    ProjetoSpring

--- src/main/java/com/example/demo/config/TestConfig.java
+++ src/main/java/com/example/demo/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.example.demo.course.entities.Category;
 import com.example.demo.course.entities.Order;
 import com.example.demo.course.entities.OrderItem;
+import com.example.demo.course.entities.Payment;
 import com.example.demo.course.entities.Product;
 import com.example.demo.course.entities.User;
 import com.example.demo.course.enums.OrderStatus;
@@ -80,8 +81,9 @@ public class TestConfig implements CommandLineRunner {
 
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
-	
-	
+		Payment pay1= new Payment(null,Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 	
 
