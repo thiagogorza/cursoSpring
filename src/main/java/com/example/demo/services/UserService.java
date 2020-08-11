a36d9b689ba33605879a3bd1658c6e5bfbb3d0c7
thiagogorza <thiagomgorza@gmail.com>
2020-08-11 14:40:00 -0300

    ProjetoSpring

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.course.entities.User;
@@ -15,39 +16,41 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User>findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
-		
-	Optional<User> obj= repository.findById(id);
-	return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
-	
+
 	public User update(Long id, User obj) {
 		User entity = repository.getOne(id);
-	updateData(entity,obj);
-	return repository.save(entity);
-	
-	
+		updateData(entity, obj);
+		return repository.save(entity);
+
 	}
 
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-		
-	
+
 	}
-	
+
 }
